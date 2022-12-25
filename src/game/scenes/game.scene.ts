6d144818584b585b6ac892eb988eeb158c6c9322
Robouste <Robouste@users.noreply.github.com>
2022-12-25 16:06:17 +0100

    Initial commit

---
+++ src/game/scenes/game.scene.ts
@@ -0,0 +1,17 @@
+import { Keys } from "../helpers/keys";
+
+export class GameScene extends Phaser.Scene {
+	public image: Phaser.GameObjects.Image;
+
+	constructor() {
+		super(Keys.Scenes.Game);
+	}
+
+	public create(): void {
+		this.image = this.add.image(400, 300, "logo");
+	}
+
+	public update(): void {
+		this.image.rotation += 0.01;
+	}
+}
